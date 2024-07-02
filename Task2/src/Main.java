3f6caafe591f824ff5108bc873f094abfdf019ba
Licesur <Sovakod@gmail.com>
2024-07-02 11:52:17 +0300

    Solved

--- Task2/src/Main.java
+++ Task2/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //todo код писать тут. По желанию(рекомендуется) вынести код в методы.
+       refactured(new Scanner(System.in).nextLine());
+    }
+    public static boolean isFormat(String input){
+        if((input.indexOf(' ') == input.lastIndexOf(" ")) ||
+                (input.substring(input.indexOf(' ')+1,
+                        input.lastIndexOf(" ")).indexOf(' ') != -1)) return false;
+        else return true;
+        }
+    public static void refactured(String input) {
+        if (isFormat(input)) {
+            String firstName = input.substring(0, input.indexOf(' '));
+            String secondName = input.substring(input.indexOf(' ') + 1, input.lastIndexOf(" "));
+            String thirdName = input.substring(input.lastIndexOf(" ") + 1, input.length());
+            System.out.println("Фамилия: " + firstName + "\n"
+                    + "Имя: " + secondName + "\n"
+                    + "Отчество: " + thirdName + "\n");
+        }
+        else System.out.println("incorrect format");
     }
 }
